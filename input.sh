d16bf77958a1711af92b06a024148d8a0ee4db0e
sunnym97 <15201030sunny@viva-technology.org>
2019-11-11 18:26:13 +0530

    test2added

--- input.sh
+++ input.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -x
 
+declare -A TESTC
 read -p "Enter the value of a" a
 read -p "Enter the value of b" b
 read -p "Enter the value of c" c
 echo $a $b $c
-d=$(($a+$b*$c))
-echo $d
+TESTC[test1]=$(($a + $b * $c))
+TESTC[test2]=$(($c + $a/$b))
+
+echo ${TESTC[test2]}
